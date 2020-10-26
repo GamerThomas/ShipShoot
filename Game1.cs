53350ae479e3bef4b5b3b631200105f02f2b8f1f
l3w1s1234 <32579126+l3w1s1234@users.noreply.github.com>
2020-10-26 10:26:12 +0000

    Initial commit

---
+++ Game1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShipShoot
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Sprite playerSprite;
+        Sprite backgroundSprite;
+
+        Point screenSize = new Point(800,450);
+        Texture2D background, missile, saucer;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = screenSize.X;
+            _graphics.PreferredBackBufferHeight = screenSize.Y;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            saucer = Content.Load<Texture2D>("saucer");
+            missile = Content.Load<Texture2D>("missile2");
+            background = Content.Load<Texture2D>("backG");
+
+            backgroundSprite = new Sprite(background, new Vector2(0, 0));
+            playerSprite = new PlayerSprite(saucer, new Vector2(screenSize.X/6,screenSize.Y/2));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            playerSprite.Update(gameTime, screenSize);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            _spriteBatch.Begin();
+
+            backgroundSprite.Draw(_spriteBatch);
+             playerSprite.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ShipShoot
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
