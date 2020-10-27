8a94583eb2ecbe93890f3d77afc802af81621130
l3w1s1234 <32579126+l3w1s1234@users.noreply.github.com>
2020-10-27 15:57:31 +0000

    27/10/2020
    Work done on this day

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 
 namespace ShipShoot
 {
@@ -9,12 +13,15 @@ namespace ShipShoot
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Sprite playerSprite;
+        PlayerSprite playerSprite;
         Sprite backgroundSprite;
+        List<Missile> missileSprite = new List<Missile>();
+        SpriteFont uiFont;
 
         Point screenSize = new Point(800,450);
-        Texture2D background, missile, saucer;
+        Texture2D backgroundTxr, missileTxr, saucerTxr;
 
+        float missileTime = 2;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -35,12 +42,13 @@ namespace ShipShoot
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            saucer = Content.Load<Texture2D>("saucer");
-            missile = Content.Load<Texture2D>("missile2");
-            background = Content.Load<Texture2D>("backG");
+            saucerTxr = Content.Load<Texture2D>("saucer");
+            missileTxr = Content.Load<Texture2D>("missile2");
+            backgroundTxr = Content.Load<Texture2D>("backG");
+            uiFont = Content.Load<SpriteFont>("uifont");
 
-            backgroundSprite = new Sprite(background, new Vector2(0, 0));
-            playerSprite = new PlayerSprite(saucer, new Vector2(screenSize.X/6,screenSize.Y/2));
+            backgroundSprite = new Sprite(backgroundTxr, new Vector2(0, 0));
+            playerSprite = new PlayerSprite(saucerTxr, new Vector2(screenSize.X/6,screenSize.Y/2));
         }
 
         protected override void Update(GameTime gameTime)
@@ -48,9 +56,29 @@ namespace ShipShoot
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            Random rng = new Random();
+
+            if (missileTime > 0)
+            {
+                missileTime -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+
+            }
+            else if (missileSprite.Count < 5)
+            {
+                 missileSprite.Add(new Missile(missileTxr, new Vector2(screenSize.X, rng.Next(0, screenSize.Y - missileTxr.Height))));
+                missileTime = (float)(rng.NextDouble() + 0.5f);
+            }
+
             playerSprite.Update(gameTime, screenSize);
 
+            foreach (Missile missile in missileSprite) missile.Update(gameTime, screenSize);
+
+            missileSprite.RemoveAll(missile => missile.dead);
+
             base.Update(gameTime);
+
+            Debug.WriteLine(missileSprite.Count);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -62,6 +90,10 @@ namespace ShipShoot
 
             backgroundSprite.Draw(_spriteBatch);
              playerSprite.Draw(_spriteBatch);
+
+            foreach (Missile missile in missileSprite) missile.Draw(_spriteBatch);
+
+            _spriteBatch.DrawString(uiFont, "Font Test You Big Bitch!!!", new Vector2(10, 10), Color.White);
 
             _spriteBatch.End();
 
---
+++ Missile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace ShipShoot
+{
+    class Missile : Sprite
+    {
+
+        float missileSpeed = 5;
+        public bool dead = false;
+        public Missile(Texture2D newTxr, Vector2 newPos) : base(newTxr, newPos)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime, Point screenSize)
+        {
+            spritePos.X -= missileSpeed;
+
+            if (spritePos.X < -spriteTexture.Width) dead = true;
+        }
+    }
+}
